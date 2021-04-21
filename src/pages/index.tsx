70276f619810d9268439a213c24afd66855103a4
MateusJesus <mateusjdev@gmail.com>
2021-04-21 18:37:51 -0300

    ✨ add: fake API, SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,44 @@
-export default function Home() {
+// SPA
+// SSR
+// SSG
+
+import { useEffect } from "react"
+
+export default function Home(props) {
+  // Utilizando o método de SPA
+  // useEffect(() => {
+  //   fetch('http://localhost:3333/episodes')
+  //   .then(response => response.json())
+  //   .then(data => console.log(data))
+  // }, [])
+
   return (
     // HTML dentro do JS ou TS.
     <h1>Index</h1>
   )
+}
+
+// Exemplo de SSR
+// export async function getServerProps() {
+//   const response = await fetch('http://localhost:3333/episodes')
+//   const data = await response.json()
+
+//   return {
+//     props: {
+//       episodes: data,
+//     }
+//   }
+// }
+
+// Exemplo de SSG
+export async function getStatiProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
