f0829b5a211b1c1a477e7ebf33ef42def8a79879
MateusJesus <mateusjdev@gmail.com>
2021-04-23 13:51:38 -0300

    ✨ finished episode page and page dashboard

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,6 +1,7 @@
 import { format, parseISO } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import Image from 'next/image'
+import Link from 'next/link'
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { useRouter } from 'next/router'
 import { api } from '../../services/api';
@@ -28,9 +29,11 @@ export default function Episode({ episode }: EpisodeProps) {
   return (
     <div className={styles.episode}>
       <div className={styles.thumbnailContainer}>
-        <button type="button">
-          <img src="/arrow-left.svg" alt="Voltar"/>
-        </button>
+        <Link href="/">
+          <button type="button">
+            <img src="/arrow-left.svg" alt="Voltar"/>
+          </button>
+        </Link>
         <Image
           width={700}
           height={160}
