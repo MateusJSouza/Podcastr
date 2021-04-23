74aa1f72390facaab349ecc02cf00b938a75ded2
MateusJesus <mateusjdev@gmail.com>
2021-04-22 23:33:11 -0300

    ✨ add: page slug

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,9 +1,12 @@
 import { format, parseISO } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
-import { GetStaticProps } from 'next';
+import Image from 'next/image'
+import { GetStaticPaths, GetStaticProps } from 'next';
 import { useRouter } from 'next/router'
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
+
+import styles from './episode.module.scss'
 
 type Episode = {
   id: string;
@@ -13,20 +16,51 @@ type Episode = {
   duration: string;
   durationAsString: string;
   url: string;
-  publisedAt: string;
+  publishedAt: string;
   description: string;
-}
+};
 
 type EpisodeProps = {
-  
+  episode: Episode;
 }
 
-export default function Episode({ episode }) {
-  const router = useRouter();
-  
+export default function Episode({ episode }: EpisodeProps) {
   return (
-    <h1>{router.query.slug}</h1>
+    <div className={styles.episode}>
+      <div className="styles thumbnailContainer">
+        <button type="button">
+          <img src="/arrow-left.svg" alt="Voltar"/>
+        </button>
+        <Image
+          width={700}
+          height={160}
+          src={episode.thumbnail} 
+          objectFit="cover"
+        />
+        <button type="button">
+          <img src="/play.svg" alt="Tocar episódio"/>
+        </button>
+      </div>
+
+      <header>
+        <h1>{episode.title}</h1>
+        <span>{episode.members}</span>
+        <span>{episode.publishedAt}</span>
+        <span>{episode.durationAsString}</span>
+      </header>
+
+      <div
+        className={styles.description} dangerouslySetInnerHTML={{ __html: episode.description }}
+      />
+    </div>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking'
+  };
 }
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
